cd5dfc617817f9cde69f3a9447289969429d2ca0
ShougotMollik <shougotmollik@gmail.com>
2024-09-24 22:50:44 +0600

    ui design

---
+++ lib/ui/model/todo_model.dart
@@ -0,0 +1,10 @@
+class TodoModel {
+  String title;
+  bool isCompleted;
+
+  TodoModel({required this.title, required this.isCompleted});
+
+  void toggleCompleted() {
+    isCompleted = !isCompleted;
+  }
+}
