2eb5f0ae566de7918493c2a826577cb74627d18a
ShougotMollik <shougotmollik@gmail.com>
2024-09-24 22:50:20 +0600

    ui design

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffef0078);
+  static const Color secondaryColor = Color(0xfff5b6da);
+  static const Color fontColor = Color(0xfffbe2f0);
+}
